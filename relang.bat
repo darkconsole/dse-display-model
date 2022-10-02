73231ab20cd6ef50f072b5f39a705f3cbb288cdf
darkconsole <darkcee.legit@gmail.com>
2022-10-01 22:30:22 -0500

    cider stand variant for spooky month

---
+++ relang.bat
@@ -0,0 +1,10 @@
+:: this is because nobody is tranlating anything i can be lazy
+
+cp -r interface\translations\dse-display-model_ENGLISH.txt interface\translations\dse-display-model_CZECH.txt
+cp -r interface\translations\dse-display-model_ENGLISH.txt interface\translations\dse-display-model_FRENCH.txt
+cp -r interface\translations\dse-display-model_ENGLISH.txt interface\translations\dse-display-model_GERMAN.txt
+cp -r interface\translations\dse-display-model_ENGLISH.txt interface\translations\dse-display-model_ITALIAN.txt
+cp -r interface\translations\dse-display-model_ENGLISH.txt interface\translations\dse-display-model_JAPANESE.txt
+cp -r interface\translations\dse-display-model_ENGLISH.txt interface\translations\dse-display-model_POLISH.txt
+cp -r interface\translations\dse-display-model_ENGLISH.txt interface\translations\dse-display-model_RUSSIAN.txt
+cp -r interface\translations\dse-display-model_ENGLISH.txt interface\translations\dse-display-model_SPANISH.txt
